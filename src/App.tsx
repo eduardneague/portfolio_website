c25ea8d5b13752db64dc9290a034fe5c5c6b7ea4
eduardneague <eduard.neague1337@gmail.com>
2023-06-17 15:54:32 +0300

    first commit - setup (not all packages)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import  React  from 'react'
+
+const App: React.FC = (): JSX.Element => {
+
+  return (
+    <>
+      <div>
+        <h1 className = "text-orange-900 font-bold text-3xl font-[Poppins]">text</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
