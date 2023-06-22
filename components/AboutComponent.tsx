3fc28560ffcc0d757fd1557d85390a0cb70afe65
eduardneague <eduard.neague1337@gmail.com>
2023-06-22 21:31:34 +0300

    test again

--- components/AboutComponent.tsx
+++ components/AboutComponent.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../src/css_files/App.css'
 
 import CustomButton from '../components/CustomButton'
 import SmileyFace from '../components/SmileyFace'
@@ -8,15 +9,15 @@ const AboutComponent: React.FC = (): JSX.Element => {
     <>
         <div className = "rounded-2xl bg-very-light-gray h-[21rem] w-[22rem] shadow-lg flex flex-col font-[Poppins]">
             <h1 className = "text-xl font-bold ml-5 mt-5">Hello! I am</h1>    
-            {/* <h1
-                className="ml-5 font-extrabold text-transparent text-[40px] bg-clip-text bg-gradient-to-r from-black to-custom-pink"
+            <h1
+                className="eduard-text ml-5 font-extrabold text-transparent text-[40px] bg-clip-text bg-gradient-to-r from-black to-custom-pink"
             >
             Eduard Neague
-            </h1> */}
+            </h1>
             <p className = "text-[14px] leading-6 ml-5 mt-3">I am a 
                 <span className="h-[2.6rem] font-extrabold text-transparent text-[14px] bg-clip-text bg-gradient-to-r from-black to-custom-pink"> Frontend Developer </span> 
                  and <span className="h-[2.6rem] font-extrabold text-transparent text-md bg-clip-text bg-gradient-to-r from-black to-custom-pink"> UX Designer </span>
-                 based in Bucharest, Romania. <img src="romanian_flag.png" className = "h-4 inline" alt="" />
+                 based in Bucharest, Romania. <img src="romanian_flag.png" className = "romanian-flag  inline" alt="romanian_flag" />
                 <br/><br/>
                 <span> 
                     I love building apps for the web and
