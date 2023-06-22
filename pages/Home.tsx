32bcfb6b27017681a805ec6bac6ee087b24fd54f
eduardneague <eduard.neague1337@gmail.com>
2023-06-22 21:19:43 +0300

    gjkadbhlsasdgh

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import '../src/css_files/grid.css'
 
 import AboutComponent from '../components/AboutComponent'
-import Skills from '../components/Skills'
 
 const Home: React.FC = (): JSX.Element => {
   return (
     <>
         <div className = "main-grid">
           <AboutComponent/>   
-          <Skills/> 
           
         </div>
     </>
