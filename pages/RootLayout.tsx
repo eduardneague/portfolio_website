66166e08e3aa71f1a8de703919fa6eba527334c6
eduardneague <eduard.neague1337@gmail.com>
2023-06-22 21:37:03 +0300

    check navbar

--- pages/RootLayout.tsx
+++ pages/RootLayout.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 
-import NavBar from '../components/NavBar'
 
 const RootLayout: React.FC = (): JSX.Element => {
   return (
     <>
-        <NavBar/>
         <Outlet/>
     </>
   )
