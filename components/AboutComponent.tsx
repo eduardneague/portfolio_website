2cdf104619dbb36fbe8e0d88c8d789c8ee837c0b
eduardneague <eduard.neague1337@gmail.com>
2023-06-24 22:53:42 +0300

    flag...

--- components/AboutComponent.tsx
+++ components/AboutComponent.tsx
@@ -10,14 +10,14 @@ const AboutComponent: React.FC = (): JSX.Element => {
         <div className = "rounded-3xl bg-very-light-gray h-[21rem] w-[22rem] shadow-lg flex flex-col font-[Poppins]">
             <h1 className = "text-xl font-bold ml-5 mt-5">Hello! I am</h1>    
             <h1
-                className="eduard-text ml-5 font-extrabold text-transparent text-[40px] bg-gradient-to-r from-black to-custom-pink"
+                className="eduard-text ml-5 font-extrabold text-transparent text-[40px] bg-clip-text bg-gradient-to-r from-black to-custom-pink"
             >
             Eduard Neague
             </h1>
             <p className = "text-[14px] leading-6 ml-5 mt-3">I am a 
-                <span className="h-[2.6rem] font-extrabold text-transparent text-[14px]  bg-gradient-to-r from-black to-custom-pink"> Frontend Developer </span> 
-                 and <span className="h-[2.6rem] font-extrabold text-transparent text-md  bg-gradient-to-r from-black to-custom-pink"> UX Designer </span>
-                 based in Bucharest, Romania. <img src="romanian_flag.png" className = "romanian-flag  inline" alt="romanian_flag" />
+                <span className="h-[2.6rem] font-extrabold text-transparent text-[14px] bg-clip-text bg-gradient-to-r from-black to-custom-pink"> Frontend Developer </span> 
+                 and <span className="h-[2.6rem] font-extrabold text-transparent text-md bg-clip-text bg-gradient-to-r from-black to-custom-pink"> UX Designer </span>
+                 based in Bucharest, Romania. 
                 <br/><br/>
                 <span> 
                     I love building apps for the web and
