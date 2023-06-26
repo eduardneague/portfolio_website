9df8094b7f836c0c96ebca60b393bdc63a3a5695
eduardneague <eduard.neague1337@gmail.com>
2023-06-26 15:12:14 +0300

    fixed one little bug

--- pages/RootLayout.tsx
+++ pages/RootLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 
 import NavBar from '../components/NavBar'
-// dd
+
 const RootLayout: React.FC = (): JSX.Element => {
   return (
     <>
